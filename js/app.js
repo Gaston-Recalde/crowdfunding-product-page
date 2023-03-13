18ec78b589e3a2d479f1b5abdd59bc335d8ad6dc
Gaston Recalde <gastonrg94@gmail.com>
2023-03-13 10:23:37 -0300

    monitor, bookmark button event agregado

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const open = document.querySelector('.nav__toggle-open');
 const close = document.querySelector('.nav__toggle-close');
 const menu = document.querySelector('.navbar');
 
+const bookmark = document.querySelector('.button');
+const bookmarkText = document.querySelector('.button-text');
+const project = document.querySelector('.project');
+
 open.addEventListener('click', () => {
     open.style.display = 'none';
     close.style.display = 'inline-block';
@@ -12,4 +16,20 @@ close.addEventListener('click', () => {
     open.style.display = 'inline-block';
     close.style.display = 'none';
     menu.style.display = 'none';
+});
+
+
+
+// project.addEventListener('click', () => {
+
+// });
+
+bookmark.addEventListener('click', () => {
+    if(bookmark.className === 'button'){
+        bookmark.className = 'button-2';
+        bookmarkText.className = 'button-text-2'
+    } else {
+        bookmark.className = 'button';
+        bookmarkText.className = 'button-text'
+    }
 });
